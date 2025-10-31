c4d65e0accd89834363edd0a68d7e145376f420b
rishikarjn <rishikasingh2109@gmail.com>
2025-10-31 23:14:51 +0530

    change in app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,30 @@ const express=require("express");
 
 const app = express();
 
-app.use((req, res)=> {
+app.use("/",(req, res)=> {
+    res.send("Hello from the server");
+});
+
+app.use("/hello",(req, res)=> {
+    res.send("Hello from the server");
+});
+
+app.use("test",(req, res)=> {
     res.send("Hello from the server");
 });
 
 app.listen(3000, () =>{
     console.log("Server is successfully listening on port 3000...");
 });
+
+
+
+
+
+
+
+
+
 
 
 
